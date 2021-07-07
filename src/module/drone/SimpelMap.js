2a55cb5069c58110e426d384c2548456a3f699a3
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-07-07 17:00:52 +0200

    Used Map without Libary

--- src/module/drone/SimpelMap.js
+++ src/module/drone/SimpelMap.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import { ReactBingmaps } from 'react-bingmaps';
-const AnyReactComponent = ({text}) => <div>{text}</div>;
+import "react-bingmaps";
 
 class SimpleMap extends Component {
     static defaultProps = {
@@ -10,30 +9,60 @@ class SimpleMap extends Component {
 
     constructor(props) {
         super(props);
+        const obj = this;
+        window.GetMap = function () {
+            // eslint-disable-next-line no-undef
+            window.Microsoft = Microsoft;
+            obj.map = window.Microsoft.Maps.Map('#myMap', {
+                credentials: "AkBVrBtsknpJShn4Yjy9xKpdHxNdYuymoJ_1yHe95ECRs3CEIbwWmD6wje-c1R9v",
+                center: new window.Microsoft.Maps.Location(obj.props.center.latitude, obj.props.center.longitude),
+                zoom: 19,
+                mapTypeId: window.Microsoft.Maps.MapTypeId.aerial,
+
+            });
+
+            const pin = new window.Microsoft.Maps.Pushpin(new window.Microsoft.Maps.Location(obj.props.center.latitude, obj.props.center.longitude),{
+             color: 'red'
+            })
+            obj.map.entities.push(pin);
+
+        };
+    }
+
+    componentDidMount() {
+
+        let script = document.createElement("script");
+        script.type = "application/javascript";
+
+        // GetMap function will be called when Bing Maps script is downloaded, so inside there initialize your map and other params
+        script.src = "https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=AkBVrBtsknpJShn4Yjy9xKpdHxNdYuymoJ_1yHe95ECRs3CEIbwWmD6wje-c1R9v";
+
+        document.body.appendChild(script);
+
 
     }
 
-
-componentDidMount() {
-        this.forceUpdate();
-}
-
-createMapOptions(maps) {
-    return {
-        mapTypeId: 'satellite'
-
+    componentDidUpdate(prevProps, prevState, snapshot) {
     }
-}
+
+
+    createMapOptions(maps) {
+        return {
+            mapTypeId: 'satellite'
+
+        };
+    }
+
 
     render() {
         return (
-            // Important! Always set the container height explicitly
-            <div style={{height: '43em', width: '100%', marginTop: '20px'}}>
 
-                <ReactBingmaps bingmapKey="AkBVrBtsknpJShn4Yjy9xKpdHxNdYuymoJ_1yHe95ECRs3CEIbwWmD6wje-c1R9v"
+            <div>
+                {/* <ReactBingmaps bingmapKey=""
                                mapTypeId= "aerial"
                                disableStreetside={true}
                                navigationBarMode = {"compact"}
+               // AkBVrBtsknpJShn4Yjy9xKpdHxNdYuymoJ_1yHe95ECRs3CEIbwWmD6wje-c1R9v
 
 
                                pushPins = {
@@ -47,6 +76,9 @@ createMapOptions(maps) {
                                zoom = {19}
                                center = {[this.props.center.latitude, this.props.center.longitude ]}
                 />
+                */}
+                <div id="myMap" style={{height: '43em', width: '100vw', marginTop: '20px'}}/>
+
             </div>
         );
     }
