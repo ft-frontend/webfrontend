442a4855dcc655991d19703fc69cd333df9ea935
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-11-28 15:21:11 +0100

    Added file rendering

--- src/module/cloud/cloud.js
+++ src/module/cloud/cloud.js
@@ -3,6 +3,7 @@ import folderIcon from "../../res/folder.svg";
 import fileIcon from "../../res/file.svg";
 import api from "../../api/api";
 import { v4 as uuidv4 } from 'uuid';
+import CloudStyle from "./cloud.module.css"
 
 class Cloud extends Component {
     constructor(props) {
@@ -13,22 +14,37 @@ class Cloud extends Component {
 
         const path = urlParams.get('path')==null?"/":urlParams.get('path');
         window.history.replaceState({}, '', "/module/cloud?path="+path);
-
-        this.state = {
+        this.state ={
             path: path,
             folderContent: [],
             folderContentElements: []
-        }
+        };
+
         this.fetchFolderContent =this.fetchFolderContent.bind(this)
         this.gotoFolder =this.gotoFolder.bind(this)
         this.changeFolder =this.changeFolder.bind(this)
+        this.parseURLParams =this.parseURLParams.bind(this)
+
+        this.parseURLParams();
 
 
 
     }
+    parseURLParams() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        const path = urlParams.get('path')==null?"/":urlParams.get('path');
+        window.history.replaceState({}, '', "/module/cloud?path="+path);
+        this.setState({
+            path: path,
+            folderContent: [],
+            folderContentElements: []
+        });
+    }
 
     changeFolder(newPath) {
-        window.history.replaceState({}, '', "/module/cloud?path="+newPath);
+        window.history.pushState({}, '', "/module/cloud?path="+newPath);
     }
 
     gotoFolder(folderName) {
@@ -36,13 +52,13 @@ class Cloud extends Component {
             this.setState({
                 path: this.state.path+folderName
             })
-            window.history.replaceState({}, '', "/module/cloud?path="+this.state.path+folderName);
+            window.history.pushState({}, '', "/module/cloud?path="+this.state.path+folderName);
             this.fetchFolderContent(this.state.path+folderName);
         }else{
             this.setState({
                 path: this.state.path+"/"+folderName
             })
-            window.history.replaceState({}, '', "/module/cloud?path="+this.state.path+"/"+folderName);
+            window.history.pushState({}, '', "/module/cloud?path="+this.state.path+"/"+folderName);
             this.fetchFolderContent(this.state.path+"/"+folderName);
         }
 
@@ -52,6 +68,17 @@ class Cloud extends Component {
     componentDidMount() {
         this.fetchFolderContent(this.state.path);
 
+        this.popStateEventHandler = (event)=>{
+            this.parseURLParams();
+            this.fetchFolderContent(this.state.path);
+        };
+
+        window.addEventListener('popstate',this.popStateEventHandler)
+
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('popstate',this.popStateEventHandler)
     }
 
     fetchFolderContent(folder) {
@@ -63,10 +90,29 @@ class Cloud extends Component {
             result.foundFolder.files.forEach(content=>{
                 if(content.isFolder===true) {
 
-                    folderContentElements.push(<img onClick={()=>{this.gotoFolder(content.name)}} key={uuidv4()} src={folderIcon} height={50} width={50} alt="folder"/>)
+                    folderContentElements.push(
+
+                        <div onClick={()=>{this.gotoFolder(content.name)}} className={CloudStyle.fileEntry}>
+
+                            <div className={CloudStyle.fileEntryImageContainer}><img className={CloudStyle.fileEntryImage} key={uuidv4()} src={folderIcon}  alt="folder"/></div>
+                            <p className={CloudStyle.fileName}>{content.name}</p>
+
+                        </div>
+
+                    )
 
                 }else{
-                    folderContentElements.push(<img key={uuidv4()} src={fileIcon} height={50} width={50} alt="folder"/>)
+                    folderContentElements.push(
+
+                        <div  className={CloudStyle.fileEntry}>
+
+                            <img className={CloudStyle.fileEntryImage} key={uuidv4()} src={fileIcon}  alt="file"/>
+                            <p className={CloudStyle.fileName}>{content.name}</p>
+
+                        </div>
+
+                    )
+
 
                 }
 
@@ -88,9 +134,13 @@ class Cloud extends Component {
 
 
         return (
-            <div>
-                Ich bin die Cloud
-                {this.state.folderContentElements}
+            <div className={CloudStyle.cloudContainer}>
+
+                <div className={CloudStyle.filesContainer}>
+                    {this.state.folderContentElements}
+
+
+                </div>
             </div>
         );
     }
